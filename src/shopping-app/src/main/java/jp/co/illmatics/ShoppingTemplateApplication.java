27e46a14c7b3d8b94cac2e286fc33e5839b63a34
hara-y-illmatics <hara-y@illmatics.co.jp>
2022-12-01 18:27:24 +0900

    add spring security config

--- src/shopping-app/src/main/java/jp/co/illmatics/ShoppingTemplateApplication.java
+++ src/shopping-app/src/main/java/jp/co/illmatics/ShoppingTemplateApplication.java
@@ -19,36 +19,39 @@ import javax.persistence.PersistenceContext;
 @SpringBootApplication
 @RestController
 public class ShoppingTemplateApplication {
-  @Autowired StaffRepository repository;
+	@Autowired StaffRepository repository;
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@RequestMapping("/")
+	public String home() {
+	    return "Hello World from Docker";
+	}
 
-  @PersistenceContext
-  private EntityManager entityManager;
-
-  @RequestMapping("/")
-  public String home() {
-      return "Hello World from Docker";
-  }
-  @RequestMapping("/staff")
-  public String staff() {
-      Optional<Staff> optional =  repository.findById(1);
-      Staff staff = optional.orElseGet(() -> new Staff());
-      return "Hello " + staff.getName();
-  }
-
-  @RequestMapping("/query")
-  public String query() {
-      List<Staff> results = entityManager
-            .createNativeQuery("select * from staff where emp_id = :id ", Staff.class)
-            .setParameter("id",1)
-            .getResultList();
-      String name = "";
-
-      if(results.size() != 0){
-        name = results.get(0).getName();
-      }
-      return "Hello " + name;
-  }
-	public static void main(String[] args) {
+	/*  @RequestMapping("/staff")
+	  public String staff() {
+	  Optional<Staff> optional =  repository.findById(1);
+	  Staff staff = optional.orElseGet(() -> new Staff());
+	  return "Hello " + staff.getName();
+	  }
+	
+	  @RequestMapping("/query")
+	  public String query() {
+	  List<Staff> results = entityManager
+	        .createNativeQuery("select * from staff where emp_id = :id ", Staff.class)
+	        .setParameter("id",1)
+	        .getResultList();
+	  String name = "";
+	
+	  if(results.size() != 0){
+	    name = results.get(0).getName();
+	  }
+	  return "Hello " + name;
+	  }
+	*/
+  
+  	public static void main(String[] args) {
 		SpringApplication.run(ShoppingTemplateApplication.class, args);
 	}
 }
